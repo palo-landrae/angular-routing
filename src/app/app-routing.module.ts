64600f87488b7b45cea46a3a0ace342fa7b7d7d2
unknown <louiandraepalo@gmail.com>
2022-04-01 22:08:59 +0200

    :construction:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnimalsComponent } from './animals/animals.component';
+import { FruitsComponent } from './fruits/fruits.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'animals', component: AnimalsComponent},
+  { path: 'fruits', component: FruitsComponent},
+  {path: '', redirectTo: '/animals', pathMatch: 'full'},
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FruitsComponent } from './fruits/fruits.component';
+import { AnimalsComponent } from './animals/animals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FruitsComponent,
+    AnimalsComponent
   ],
   imports: [
     BrowserModule,
